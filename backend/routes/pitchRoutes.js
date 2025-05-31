6851e6d5b983ea4e9132b1c322ba7cfb45ecd2ce
kabir-dharshaaan-tech <dharshaankabir@gmail.com>
2025-05-31 21:12:56 +0530

    read_and_write

---
+++ backend/routes/pitchRoutes.js
@@ -0,0 +1,29 @@
+
+
+
+const express = require('express');
+const multer = require('multer');
+const { createPitch, getAllPitches ,putAllPitches} = require('../controllers/pitchController');
+const router = express.Router();
+
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); 
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname); 
+  },
+});
+
+const upload = multer({ storage });
+
+
+router.post('/create', upload.fields([{ name: 'image', maxCount: 1 }, { name: 'pptFile', maxCount: 1 }]), createPitch);
+
+
+router.get('/', getAllPitches);
+
+router.put('/:id',putAllPitches);
+
+module.exports = router;
